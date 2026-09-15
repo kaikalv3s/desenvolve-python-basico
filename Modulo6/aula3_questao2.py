4e3885cd8915281a2fad3d3c29d7c484029b77ac
Kaik <kaikalves1208@gmail.com>
2026-09-15 11:10:16 -0300

    Add files via upload

---
+++ Modulo6/aula3_questao2.py
@@ -0,0 +1,16 @@
+URLs = [
+    "www.google.com",
+    "www.gmail.com",
+    "www.github.com",
+    "www.reddit.com",
+    "www.yahoo.com"
+]
+
+dominios = []
+
+for url in URLs:
+    dominio = url[4:-4]
+    dominios.append(dominio)
+
+print("URLs:", URLs)
+print("Domínios:", dominios)
